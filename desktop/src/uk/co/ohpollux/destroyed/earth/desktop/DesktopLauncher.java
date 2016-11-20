eb8b6cef0fd8192d7a21c3c2958b9e7e6cdd3722
Aleks Baklanovs <baklanovs.a@gmail.com>
2016-11-20 14:45:39 +0000

    Gradle setup

---
+++ desktop/src/uk/co/ohpollux/destroyed/earth/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.co.ohpollux.destroyed.earth.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.co.ohpollux.destroyed.earth.EarthDestroyed;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EarthDestroyed(), config);
+	}
+}
